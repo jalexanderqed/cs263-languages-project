35dd38da63f9b74299b5a0925be2298ba254bb01
John Alexander <jalexander.qed@gmail.com>
2018-05-04 16:54:45 -0700

    Added pi calculation to Go code

--- go-lambda/pi.go
+++ go-lambda/pi.go
@@ -1,6 +1,24 @@
 package main
 
-import "fmt"
+import (
+       "fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type RequestClass struct{
+	InputInt int `json:"inputInt"`
+	InputString string `json:"inputString"`
+}
+
+type ResponseClass struct{
+     OutputInt int `json:"outputInt"`
+     Time1 float64 `json:"time1"`
+     Time2 float64 `json:"time2"`
+     LongTime1 int64 `json:"longTime1"`
+     LongTime2 int64 `json:"longTime2"`
+     OutputDouble float64 `json:"outputDouble"`
+     OutputString string `json:"outputString"`
+}
 
 func calcPi(iterations int) float64 {
      multiplier := 1.0
@@ -14,6 +32,14 @@ func calcPi(iterations int) float64 {
      return 4.0 * multiplier
 }
 
+func Handler(input RequestClass) (ResponseClass, error) {
+     response := ResponseClass{}
+     response.OutputDouble = calcPi(input.InputInt)
+     response.OutputString = fmt.Sprintf("Calculated Pi with %d iterations", input.InputInt)
+     
+     return response, nil
+}
+
 func main() {
-     fmt.Println("Value of pi is",calcPi(1000))
+	lambda.Start(Handler)
 }
